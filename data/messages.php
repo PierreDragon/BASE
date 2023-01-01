57e60ad3119b769b4a566fec50b3d431e298223b
PierreDragon <120688140+PierreDragon@users.noreply.github.com>
2023-01-01 18:33:56 -0500

    Add files via upload

---
+++ data/messages.php
@@ -0,0 +1,11 @@
+<?php
+$data[0][0][1]='messages';
+$data[1][0][1]='message';
+$data[1][0][2]='datetime';
+$data[1][1][1]='You have loaded demo data';
+$data[1][1][2]='2023-01-01 18:31:23';
+$data[1][2][1]='You have initialized BASE';
+$data[1][2][2]='2023-01-01 18:31:31';
+$data[1][3][1]='You have logged out!';
+$data[1][3][2]='2023-01-01 18:31:42';
+?>

28cf0c08e2038e312e8d7de096f6ce32cb10b3ad
PierreDragon <pierre.martin@live.ca>
2023-07-28 11:04:30 -0400

    data

--- data/messages.php
+++ data/messages.php
@@ -8,4 +8,8 @@ $data[1][2][1]='Add a new line';
 $data[1][2][2]='2023-07-28 09:44:52';
 $data[1][3][1]='Add a new line';
 $data[1][3][2]='2023-07-28 10:31:51';
+$data[1][4][1]='You are logged in! demo';
+$data[1][4][2]='2023-07-28 11:03:20';
+$data[1][5][1]='You have initialized BASE';
+$data[1][5][2]='2023-07-28 11:03:43';
 ?>

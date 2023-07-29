a080a78ccb81110b1fe6bfe76499b47d97795eaa
PierreDragon <pierre.martin@live.ca>
2023-07-29 12:42:34 -0400

    Suppression

--- data/messages.php
+++ data/messages.php
@@ -2,16 +2,4 @@
 $data[0][0][1]='messages';
 $data[1][0][1]='message';
 $data[1][0][2]='datetime';
-$data[1][1][1]='You have changed record 2 at the table: users';
-$data[1][1][2]='2023-07-29 10:54:57';
-$data[1][2][1]='You are logged in! demo';
-$data[1][2][2]='2023-07-29 12:18:47';
-$data[1][3][1]='You have loaded demo data';
-$data[1][3][2]='2023-07-29 12:22:13';
-$data[1][4][1]='You are logged in! demo';
-$data[1][4][2]='2023-07-29 12:29:02';
-$data[1][5][1]='The fieldname must contain only alphabetic characters and &lt;em&gt;_id&lt;&#047;em&gt; suffix for foreign key.';
-$data[1][5][2]='2023-07-29 12:31:02';
-$data[1][6][1]='The fieldname must contain only alphabetic characters and &lt;em&gt;_id&lt;&#047;em&gt; suffix for foreign key.';
-$data[1][6][2]='2023-07-29 12:31:25';
 ?>

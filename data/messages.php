b38a9e3dfdc57c0e731038078a333d3fff86458d
PierreDragon <pierre.martin@live.ca>
2023-07-29 10:47:13 -0400

    Menage 2

--- data/messages.php
+++ data/messages.php
@@ -2,4 +2,14 @@
 $data[0][0][1]='messages';
 $data[1][0][1]='message';
 $data[1][0][2]='datetime';
+$data[1][1][1]='You are logged in! pierre';
+$data[1][1][2]='2023-07-29 10:39:13';
+$data[1][2][1]='You have deleted record: 6 in the table: users';
+$data[1][2][2]='2023-07-29 10:39:26';
+$data[1][3][1]='You have deleted record: 5 in the table: users';
+$data[1][3][2]='2023-07-29 10:39:29';
+$data[1][4][1]='You have deleted record: 4 in the table: users';
+$data[1][4][2]='2023-07-29 10:39:32';
+$data[1][5][1]='You have deleted record: 3 in the table: users';
+$data[1][5][2]='2023-07-29 10:39:35';
 ?>

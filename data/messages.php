fcefab6b00ddd7b0f72ffd86d2dd763a0a142da7
PierreDragon <pierre.martin@live.ca>
2023-07-29 12:28:02 -0400

    Ajout de config
    Creation du fichier config.php

--- data/messages.php
+++ data/messages.php
@@ -2,14 +2,10 @@
 $data[0][0][1]='messages';
 $data[1][0][1]='message';
 $data[1][0][2]='datetime';
-$data[1][1][1]='You are logged in! pierre';
-$data[1][1][2]='2023-07-29 10:39:13';
-$data[1][2][1]='You have deleted record: 6 in the table: users';
-$data[1][2][2]='2023-07-29 10:39:26';
-$data[1][3][1]='You have deleted record: 5 in the table: users';
-$data[1][3][2]='2023-07-29 10:39:29';
-$data[1][4][1]='You have deleted record: 4 in the table: users';
-$data[1][4][2]='2023-07-29 10:39:32';
-$data[1][5][1]='You have deleted record: 3 in the table: users';
-$data[1][5][2]='2023-07-29 10:39:35';
+$data[1][1][1]='You have changed record 2 at the table: users';
+$data[1][1][2]='2023-07-29 10:54:57';
+$data[1][2][1]='You are logged in! demo';
+$data[1][2][2]='2023-07-29 12:18:47';
+$data[1][3][1]='You have loaded demo data';
+$data[1][3][2]='2023-07-29 12:22:13';
 ?>
--- index.php
+++ index.php
@@ -1,32 +1,5 @@
 <?php
-declare(strict_types=1);
-session_start();
-error_reporting(E_ALL);
-// display_errors : 1 or off 
-ini_set("display_errors", "1");
-set_time_limit(60); //seconds
-date_default_timezone_set('America/New_York'); 
-
-define('VERSION','2.0');
-define('WEBROOT',str_replace('index.php','',$_SERVER['SCRIPT_NAME']));
-define('ROOT',str_replace('index.php','',$_SERVER['SCRIPT_FILENAME']));
-define('DEFAULTCONTROLLER','main');
-define('DEFAULTDATABASE','data');
-define('CLASSDIRECTORY',ROOT.'classes/');
-define('DATADIRECTORY',ROOT.'data/');
-define('VIEWDIRECTORY',ROOT.'views/');
-define('ASSETDIRECTORY',WEBROOT.'assets/');
-define('CONTROLLER',0);
-define('ACTION',1);
-define('PRIMARY',1);
-define('TABLE',2);
-define('LINE',2);
-define('FIELD',3);
-define('INDEX',3);
-define('VALUE',4);
-
-require(ROOT.'core/model.php');
-require(ROOT.'core/controller.php');
+include("config.php");
 
 if (isset($_GET['jumbo'])) {$_SESSION['jumbo'] = $_GET['jumbo'];}
 		

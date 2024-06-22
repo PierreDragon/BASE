8008f6755d8c06785fe5b094dc9f04b2130ab337
PierreDragon <pierre.martin@live.ca>
2024-06-22 11:12:20 -0400

    Update details.php

--- views/system/details.php
+++ views/system/details.php
@@ -6,25 +6,18 @@
 	  <h6>Model <span class="badge"> <?=Core\Model::$version?> </span></h6>
 	  <h6>PHP Version  <span class="badge"><?php echo phpversion(); ?></span></h6>
   </div>
-	<div class="panel-body">	
-		<ul>
-		<li>Last review: 2024-06-16 19:08</li>
-		<li>@added function add_record():int</li>
-		<li>@added function primary_column()</li>
-		<li>@optimized function set_line()</li>
-		</ul>
+	<div class="panel-body">
+<pre>	
+/**
+* @class: Controller
+* @version:	10.5
+* @author: info@webiciel.ca
+* @php: 8
+* @revision: 2024-06-22 11:04
+* @optimization of fields list customization and system file as well
+* @control of the visibility of the id_* in dropdown list
+* @licence MIT
+*/
+</pre>	
 	</div>
 </div>
-<!--<div class="panel panel-primary">
-  <div class="panel-heading">Database : <?php echo '<strong>'.$file.'</strong>'; ?></div>
-	  <div class="panel-body">
-		  <ul>
-			<li>File size : <?php echo $ffilesize; ?> octets</li>
-			<li>Import | Export : [<em>.php</em>, <em>.ser</em>, <em>.json</em>]</li>
-			<li>Symmetry : <?php echo '['.$numtables.']['.$maxlines.']['.$maxcols.']'; ?></li>
-			<li>Number of tables : <?php echo $numtables; ?></li>
-			<li>Number of lines : <?php echo $maxlines; ?></li>
-			<li>Number of columns : <?php echo $maxcols; ?></li>
-		  <ul>
-	  </div>
-</div>-->

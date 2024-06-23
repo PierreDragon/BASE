27878f627a5ef44a9cad1ce33ab7a614791c939f
PierreDragon <pierre.martin@live.ca>
2024-06-23 19:37:03 -0400

    CSS

--- data/messages.php
+++ data/messages.php
@@ -4,4 +4,8 @@ $data[1][0][1]='id_message';
 $data[1][0][2]='message';
 $data[1][0][3]='user';
 $data[1][0][4]='datetime';
+$data[1][1][1]='1';
+$data[1][1][2]='Create your own base ! | &lt;a href="&#047;base&#047;login"&gt;Login&lt;&#047;a&gt;';
+$data[1][1][3]='';
+$data[1][1][4]='2024-06-23 17:27:00';
 ?>
